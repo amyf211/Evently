efb43c66e083086ccb704ba48b56d2877608f31d
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-11-11 21:02:08 +0000

    updated cors origin to netlify url

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const {
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://evently2024.netlify.app' }));
 app.use(express.json());
 
 app.get('/api/events', getEventsController);               
