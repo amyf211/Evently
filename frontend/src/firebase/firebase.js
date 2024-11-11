8c3949151f778e9a52f52090c74d9e947a7b0123
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-11-11 18:39:30 +0000

    fixed some imports

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "/firebase/app";
+import { getAuth } from "/firebase/auth";
+import { getFirestore } from "/firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
