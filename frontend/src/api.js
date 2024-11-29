607c8109a763ba5e0b15ea90ea27ddf1c604aa52
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-11-29 12:10:39 +0000

    updated backend urls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const fetchEvents = async () => {
-  const response = await axios.get(`http://https://evently-km2e.onrender.com/api/events`);
+  const response = await axios.get(`https://evently-km2e.onrender.com/api/events`);
   console.log(response.data)
   return response.data;
 };
 
 export const fetchEventById = async (id) => {
   try {
-      const response = await axios.get(`http://https://evently-km2e.onrender.com/api/events/${id}`);
+      const response = await axios.get(`https://evently-km2e.onrender.com/api/events/${id}`);
       return response.data;
   } catch (error) {
       console.error('Error fetching event:', error);
--- frontend/src/components/CreateEvent.jsx
+++ frontend/src/components/CreateEvent.jsx
@@ -30,7 +30,7 @@ const CreateEvent = () => {
     console.log("Data to send:", dataToSend);
 
     try {
-      const response = await axios.post('http://https://evently-km2e.onrender.com/api/create-events', dataToSend);
+      const response = await axios.post('https://evently-km2e.onrender.com/api/create-events', dataToSend);
       console.log('Event created successfully:', response.data);
 
       setEventData({
