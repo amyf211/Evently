2af0980a3cfbde3347d9bd2f2922df441f5bf2aa
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-11-29 11:57:27 +0000

    updated frontend from localhost to hosted backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const fetchEvents = async () => {
-  const response = await axios.get(`http://localhost:5000/api/events`);
+  const response = await axios.get(`http://https://evently-km2e.onrender.com/api/events`);
   console.log(response.data)
   return response.data;
 };
 
 export const fetchEventById = async (id) => {
   try {
-      const response = await axios.get(`http://localhost:5000/api/events/${id}`);
+      const response = await axios.get(`http://https://evently-km2e.onrender.com/api/events/${id}`);
       return response.data;
   } catch (error) {
       console.error('Error fetching event:', error);
